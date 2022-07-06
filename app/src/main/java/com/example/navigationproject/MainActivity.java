0bec82c0de6d88c8b329f5533636f94134eb5afc
Krishmi Sangtani <krishmisangtani92@gmail.com>
2022-07-06 16:20:52 +0530

    Toast added

--- app/src/main/java/com/example/navigationproject/MainActivity.java
+++ app/src/main/java/com/example/navigationproject/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, Activity1.class));
+                Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
             }
         });
 
